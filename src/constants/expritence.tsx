418887ed094fde24038127ed481396127cb4c8b4
singhnikhil01 <singhsansar433@gmail.com>
2025-11-24 08:40:33 +0530

    changed exprience

--- src/constants/expritence.tsx
+++ src/constants/expritence.tsx
@@ -1,24 +1,51 @@
 export const experienceData = {
-    title: "My Career Path",
-    subtitle: "From intern to innovator – a journey of growth and learning.",
-    timeline: [
-      {
-        role: "Research Intern",
-        company: "Amrita Create Lab",
-        duration: "Jan 2025 - Present",
-        contributions: [
-          "Developed a Model to generate synthetic Health Records",
-          "Worked on creating Virtual Patient",
-          "Implement the custom module for Graph to Graph Generation"
-        ],
-        technologies: ["Python","Knowledge Graph","Neo4j","MIMIC-IV", "VAEs","Transformers","GNN"]
-      }
-    ],
-    growthMetrics: [
-      { label: "Code Quality", value: 80, color: "bg-orange-500" },
-      { label: "Team Collaboration", value: 90, color: "bg-blue-500" },
-      { label: "Problem Solving", value: 95, color: "bg-orange-500" }
-    ],
-    ctaText: "Ready to leverage my skills for impactful projects!",
-    ctaButton: "Let's Connect & Create",
-  };
+  title: "My Career Path",
+  subtitle: "From intern to innovator – a journey of growth and learning.",
+  timeline: [
+    {
+      role: "Research Intern",
+      company: "Amrita Create Lab",
+      duration: "Jan 2025 - May 2025",
+      contributions: [
+        "Developed a Model to generate synthetic Health Records",
+        "Worked on creating Virtual Patient",
+        "Implement the custom module for Graph to Graph Generation",
+      ],
+      technologies: [
+        "Python",
+        "Knowledge Graph",
+        "Neo4j",
+        "MIMIC-IV",
+        "VAEs",
+        "Transformers",
+        "GNN",
+      ],
+    },
+    {
+      role: "Jr Project Engineer",
+      company: "Amrita Create Lab",
+      duration: "July 2025 - september 2025",
+      contributions: [
+        "Engineered  LLM- and VLM-based solutions for assistive communication, sign language translation, Augmentative and Alternative Communication AAC systems, and multilingual educational tools. ",
+        " Integrating speech, text, visual, and multimodal data to build inclusive AI platforms that support accessibility in education and healthcare. ",
+        "Collaborating with interdisciplinary teams across education, healthcare, and disability studies to co-create impactful AI systems",
+      ],
+      technologies: [
+        "Python",
+        "Knowledge Graph",
+        "Neo4j",
+        "MIMIC-IV",
+        "VAEs",
+        "Transformers",
+        "GNN",
+      ],
+    },
+  ],
+  growthMetrics: [
+    { label: "Code Quality", value: 80, color: "bg-orange-500" },
+    { label: "Team Collaboration", value: 90, color: "bg-blue-500" },
+    { label: "Problem Solving", value: 95, color: "bg-orange-500" },
+  ],
+  ctaText: "Ready to leverage my skills for impactful projects!",
+  ctaButton: "Let's Connect & Create",
+};
